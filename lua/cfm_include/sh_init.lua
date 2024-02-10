d5280d0c884fecc274928f0de00d46af98d15b3b
shockpast <shockpast@gmail.com>
2024-02-10 16:38:54 +0300

    fix!: minor improvements and fixes

--- lua/cfm_include/sh_init.lua
+++ lua/cfm_include/sh_init.lua
@@ -1,48 +1,47 @@
-CFM = CFM or {}
-CFM.Include = CFM.Include or {}
+CFMInclude = CFMInclude or {}
+CFMInclude.Version = "10/02/2024"
 
-function CFM.Include.AddDirectory( sDir, sPath, sSorting, bRecursive )
-  local files, dirs = file.Find( sDir, sPath, sSorting )
+function CFMInclude.AddFile( sFileName )
+  local state = string.sub( sFileName, 1, 3 )
 
-  local _dir = string.Explode( "/", sDir )
-  sDir = table.remove( _dir, #_dir - 1 )
+  if state == "sv_" then
+    include( sFileName )
+  end
+
+  if state == "cl_" then
+    if SERVER then
+      AddCSLuaFile( sFileName )
+    end
+
+    if CLIENT then
+      include( sFileName )
+    end
+  end
+
+  if state == "sh_" then
+    if SERVER then
+      AddCSLuaFile( sFileName )
+      include( sFileName )
+    end
+
+    include( sFileName )
+  end
+end
+
+function CFMInclude.AddDirectory( sDir, sSorting, bRecursive )
+  if string.Right( sDir, 1 ) ~= "/" then sDir = sDir .. "/" end
+
+  local files, dirs = file.Find( sDir .. "*", "LUA", sSorting )
 
   if #files > 0 then
     for _, fileName in ipairs( files ) do
-      if string.sub( fileName, #fileName - 3, #fileName ) ~= "lua" then goto nope end
-
-      local state = string.sub( fileName, 1, 3 )
-
-      if state == "sv_" then
-        include( sDir .. fileName )
-      end
-
-      if state == "cl_" then
-        if SERVER then
-          AddCSLuaFile( sDir .. fileName )
-        end
-
-        if CLIENT then
-          include( sDir .. fileName )
-        end
-      end
-
-      if state == "sh_" then
-        if SERVER then
-          AddCSLuaFile( sDir .. fileName )
-          include( sDir .. fileName )
-        end
-
-        include( sDir .. fileName )
-      end
-
-      ::nope::
+      CFMInclude.AddFile( sDir .. fileName )
     end
   end
 
   if bRecursive and #dirs > 0 then
     for _, dirName in ipairs( dirs ) do
-      CFM.Include.AddDirectory( sDir .. dirName, sPath )
+      CFMInclude.AddDirectory( sDir .. dirName, sSorting, bRecursive )
     end
   end
 end
